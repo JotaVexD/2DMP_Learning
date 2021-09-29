d28932e2895a3b1f56909842fe761d9af70f9d79
JotaVe <jotavexd@gmail.com>
2021-09-29 19:46:01 -0300

    Login First Commit(Raw)

---
+++ Assets/Scripts/Menu/UI/LoginUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginUI : MonoBehaviour
+{
+    public GameObject LoginPanel;
+    public GameObject RegisterPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
